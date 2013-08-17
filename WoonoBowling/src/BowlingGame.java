0cbad217aaf2444d236b0c209562775fb43183ce
woonohyo <wonhyo.yi@gmail.com>
2013-08-17 18:44:57 +0900

    #3. Roll 메소드 구현. Roll을 할 때마다 프레임과 투구수가 제대로 변경되는가?

--- WoonoBowling/src/BowlingGame.java
+++ WoonoBowling/src/BowlingGame.java
@@ -1,12 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class BowlingGame {
-	
+
 	private int currentFrame;
 	private int currentRoll;
-	
+	private int currentPin;
+	private int totalRoll = 0;
+
+	// (currentFrame, currentRoll)
+	// 편의를 위해 실제 프레임/투구수와 2차원 배열 내에 위치하는 [][]의 숫자는 동일하게 설정합니다.
+	// 따라서 2차원 배열의 크기는 [0][0]을 제외하기 때문에 [11][4]가 됩니다.
+	private int[][] frameRoll = new int[11][4];
+
+	// 2차원 배열 (frame, roll)을 Key값으로 갖고, 스코어를 value로 갖는 hashmap입니다.
+	private Map<Object, Character> scoreTable = new HashMap<Object, Character>();
+
+	// Frame을 Key값을 갖고, 스코어를 value로 갖는 hashmap입니다.
+	private Map<Integer, Integer> frameScore = new HashMap<Integer, Integer>();
+
 	public BowlingGame() {
 		currentFrame = 1;
 		currentRoll = 1;
+		currentPin = 10;
 	}
 
 	public int getCurrentFrame() {
@@ -17,4 +33,62 @@ public class BowlingGame {
 		return currentRoll;
 	}
 
+	public void roll(int pinDown) {
+		System.out.println(currentFrame + "프레임 " + currentRoll + "번째 투구");
+		System.out.println("공을 굴립니다! 데구르르르....");
+		totalRoll++;
+		currentPin = currentPin - pinDown;
+		
+		System.out.println(pinDown + "핀을 쳤습니다!");
+		
+		if (currentRoll == 1 && pinDown == 10) {
+			System.out.println("대단해요! 스트라이크입니다!");
+			putScoreTable('X');
+			goNextFrame();
+		}
+
+		else if (currentRoll == 2 && currentPin == 0) {
+			System.out.println("멋진 스페어처리군요.");
+			putScoreTable('/');
+			goNextFrame();
+		}
+		
+		else if ( currentRoll == 1 && pinDown == 0 ) {
+			System.out.println("초구부터 거터라니ㅠ");
+			putScoreTable('-');
+			goNextRoll();
+		}
+		
+		else if ( currentRoll == 2 && pinDown == 0 ) {
+			System.out.println("두번째가 거터라니. 슬프네요.");
+			putScoreTable('-');
+			goNextFrame();
+		}
+		
+		else if ( currentRoll == 2 ) {
+			putScoreTable((char)pinDown);
+			goNextFrame();
+		}
+
+		else {
+			goNextRoll();
+		}
+	}
+
+	private void goNextRoll() {
+		currentRoll++;
+		System.out.println("다음 공을 던질 준비를 합니다.");
+		System.out.println("다음번에 " + currentPin + "핀을 치면 스페어처리를 할 수 있어요! 힘내세요!\n");
+	}
+
+	private void goNextFrame() {
+		currentFrame++;
+		currentRoll = 1;
+		currentPin = 10;
+		System.out.println("수고했습니다! 다음 프레임으로 넘어갑니다!\n\n");
+	}
+
+	private void putScoreTable(char pinDown) {
+			scoreTable.put(frameRoll[currentFrame][currentRoll], pinDown);
+	}
 }
--- WoonoBowling/test/BowligGameTest.java
+++ WoonoBowling/test/BowligGameTest.java
@@ -1,18 +1,44 @@
 import junit.framework.TestCase;
 
-
 public class BowligGameTest extends TestCase {
-	
+
 	private BowlingGame game;
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		game = new BowlingGame();
 	}
-	
+
 	public void testCreate() throws Exception {
-		assertEquals(1, game.getCurrentFrame());
-		assertEquals(1, game.getCurrentRoll());
+		assertEquals(1, game.getCurrentFrame()); assertEquals(1, game.getCurrentRoll());
 	}
 
+	// 공을 던지는 메소드 Roll에 의해 프레임과 투구수가 제대로 작동하는지 테스트합니다.
+	// 스트라이크, 스페어, 거터 등,
+	// 각종 케이스에 대해서도 제대로 작동하는지 확인합니다.
+	// 공을 굴리기 전에 현재 프레임과 투구수가 결정이 되어 있습니다.
+	public void testRoll() throws Exception {
+		assertEquals(1, game.getCurrentFrame()); assertEquals(1, game.getCurrentRoll());
+		game.roll(10);
+
+		assertEquals(2, game.getCurrentFrame()); assertEquals(1, game.getCurrentRoll());
+		game.roll(7);
+		assertEquals(2, game.getCurrentFrame()); assertEquals(2, game.getCurrentRoll());
+		game.roll(3);
+
+		assertEquals(3, game.getCurrentFrame()); assertEquals(1, game.getCurrentRoll());
+		game.roll(0);
+		assertEquals(3, game.getCurrentFrame()); assertEquals(2, game.getCurrentRoll());
+		game.roll(0);
+
+		assertEquals(4, game.getCurrentFrame()); assertEquals(1, game.getCurrentRoll());
+		game.roll(3);
+		assertEquals(4, game.getCurrentFrame()); assertEquals(2, game.getCurrentRoll());
+		game.roll(0);
+
+		assertEquals(5, game.getCurrentFrame()); assertEquals(1, game.getCurrentRoll());
+		game.roll(0);
+		assertEquals(5, game.getCurrentFrame()); assertEquals(2, game.getCurrentRoll());
+		game.roll(10);
+	}
 }
